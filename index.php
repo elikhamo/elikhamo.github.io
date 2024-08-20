1bd7b344a7480dfce70d33616b2359274369bec1
elikhamo <168079537+elikhamo@users.noreply.github.com>
2024-08-20 15:12:14 +0800

    update

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+      content="High Quality Artwork Market with Content-Based Filtering for Personalized Recommendations - filartdemo.github.io">
+
+    <!-- Open Graph protocol -->
+    <meta property="og:title" content="E-Likha">
+    <meta property="og:site_name" content="E-Likha">
+    <meta property="og:description"
+      content="High Quality Artwork Market with Content-Based Filtering for Personalized Recommendations - filartdemo.github.io">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://filartdemo.github.io/">
+    <meta property="og:image"
+      content="https://filartdemo.github.io/assets/FilArtDemoPreview.png">
+    <meta property="og:image:secure_url"
+      content="https://filartdemo.github.io/assets/FilArtDemoPreview.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1471">
+    <meta property="og:image:height" content="981">
+    <meta property="og:image:alt"
+      content="FilArt">
+
+    <!-- import from fonts.google.com -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Bree+Serif&family=Caprasimo&family=Niconne&family=Noto+Color+Emoji&family=Noto+Sans+JP:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Pacifico&family=Walter+Turncoat&display=swap"
+      rel="stylesheet">
+
+    <!-- import from getbootstrap.com -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- import  -->
+    <link href="css/styles.css" rel="stylesheet">
+
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+    <title>E-Likha</title>
+  </head>
+  <body>
+    <!-- nav -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="./">E-Likha</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse" aria-controls="navbarCollapse"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page"
+                href="./">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="explore.html">Explore</a>
+            </li>
+          </ul>
+          <a href="account_login.html">
+            <button class="btn btn-outline-success" type="submit">Login</button>
+          </a>
+        </div>
+      </div>
+    </nav>
+
+    <!-- main -->
+    <main class="m-2">
+      <div>
+        <h1>hey guys hahaha</h1>
+        <em>Hello</em>
+      </div>
+    </main>
+
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- import script -->
+    <script src="js/script.js"></script>
+  </body>
+</html>
